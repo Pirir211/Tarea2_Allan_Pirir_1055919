0bdca305a3cf1d85a761b88eae74a447485fb312
Pirir211 <enmanuelcanel.15@gmail.com>
2020-09-14 21:13:14 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2_Allan_Pirir_1055919/MyForm.cpp
@@ -0,0 +1,16 @@
+#include "MyForm.h"
+
+
+using namespace System;
+using namespace::Windows::Forms;
+[STAThreadAttribute]
+
+
+int main(array<String^>^ arg)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Tarea2AllanPirir1055919::MyForm  mainForm;
+	Application::Run(% mainForm);
+	return 0;
+}
